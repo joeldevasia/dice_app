b165277656f9c5dd0293d6145ea4b17c9d5464a5
Joel Devasia <joeldevasia1962002@gmail.com>
2022-06-15 20:07:56 +0530

    It's now fully functional

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'dart:math';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -6,20 +7,69 @@ void main() {
       home: Scaffold(
         backgroundColor: Colors.red,
         appBar: AppBar(
-          title: Text('Dicee'),
+          title: const Text('Dice App'),
+          centerTitle: true,
           backgroundColor: Colors.red,
         ),
-        body: DicePage(),
+        body: const DicePage(),
       ),
     ),
   );
 }
-
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
   const DicePage({Key? key}) : super(key: key);
 
   @override
+  State<DicePage> createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftDiceNum = 1;
+  int rightDiceNum = 1;
+
+  @override
   Widget build(BuildContext context) {
-    return Container();
+    return Center(
+      child: Row(
+        children: [
+          Expanded(
+            child: TextButton(
+              style:
+              TextButton.styleFrom(splashFactory: NoSplash.splashFactory),
+              onPressed: () {
+                updateDiceNum();
+                var snackBar = const SnackBar(
+                  content: Text('Left Dice Rolled'),
+                );
+                ScaffoldMessenger.of(context).clearSnackBars();
+                ScaffoldMessenger.of(context).showSnackBar(snackBar);
+              },
+              child: Image.asset('images/dice$leftDiceNum.png'),
+            ),
+          ),
+          Expanded(
+            child: TextButton(
+              style:
+              TextButton.styleFrom(splashFactory: NoSplash.splashFactory),
+              onPressed: () {
+                updateDiceNum();
+                var snackBar = const SnackBar(
+                  content: Text('Right Dice Rolled'),
+                );
+                ScaffoldMessenger.of(context).clearSnackBars();
+                ScaffoldMessenger.of(context).showSnackBar(snackBar);
+              },
+              child: Image.asset('images/dice$rightDiceNum.png'),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+  void updateDiceNum(){
+    setState(() {
+      leftDiceNum = Random().nextInt(6) + 1;
+      rightDiceNum = Random().nextInt(6) + 1;
+    });
   }
 }
